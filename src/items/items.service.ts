3cca408889761e3e745e9fed7b500e9a23cf4e20
Sanne <sannelindholm@hotmail.com>
2021-03-04 15:27:09 +0100

    Adding service methods

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -34,3 +34,34 @@ let items: Items = {
 /**
  * Service Methods
  */
+export const findAll = async (): Promise<Item[]> => Object.values(items);
+
+export const find = async (id: number): Promise<Item> => items[id];
+
+export const create = async (newItem: BaseItem): Promise<Item> => {
+  const id = new Date().valueOf();
+  items[id] = {
+    id,
+    ...newItem,
+  };
+  return items[id];
+};
+
+export const update = async (
+  id: number,
+  itemUpdate: BaseItem
+): Promise<Item | null> => {
+  const item = await find(id);
+  if (!item) {
+    return null;
+  }
+  items[id] = { id, ...itemUpdate };
+  return items[id];
+};
+
+export const remove = async (id: number): Promise<null | void> => {
+  const item = await find(id)
+  if (!item) {
+    return null
+  }
+}
